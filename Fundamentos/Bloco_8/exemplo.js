4b487b376ace3697d7b3c9eb71a8fd41ce8104a6
João V. Dias <diasvictorj@gmail.com>
2021-09-09 10:40:43 -0300

    Bloco 8

---
+++ Fundamentos/Bloco_8/exemplo.js
@@ -0,0 +1,6 @@
+// Ao chamar a função doingThings:
+const doingThings(wakeUp) => ('Acordando!!') ;
+
+// Ela deve retornar o valor do respectivo parâmetro, neste caso:
+// Acordando!!
+console.log(doingThings(wakeUp));
